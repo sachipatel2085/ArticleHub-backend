45cf545d72e8f5abfa601d29f7088e26aecf22de
sachipatel <sachixl300@gmail.com>
2026-01-17 16:03:40 +0530

    fix mongoDB connection for render

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,14 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "articlehub",
-    });
-    console.log("Database connected successfully");
-  } catch (error) {
-    console.error("MongoDB connection failed:", error);
-    process.exit(1);
-  }
-};
+    try {
+        console.log("MONGO_URI from env:", process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("database is connected");
+    } catch (error) {
+        console.error("Mongodb connection failed:",error.massage);
+        
+    }
+}
 
 export default connectDB;
