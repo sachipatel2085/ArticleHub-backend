5ea185120e0006473b0caa85c9891867896048b0
sachipatel <sachixl300@gmail.com>
2026-01-17 14:44:20 +0530

    fix mongoDB env variable and render deployment issue

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
+        console.log("MONGO_URI from env:", process.env.MONGO_URI)
         await mongoose.connect(process.env.MONGO_URI);
         console.log("database is connected");
     } catch (error) {
