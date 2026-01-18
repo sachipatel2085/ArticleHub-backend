9c704f4d51c9efa1ab1d11e512480444af573eeb
sachipatel <sachixl300@gmail.com>
2026-01-18 16:44:32 +0530

    vercle cors error

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
+  "https://articlehub.vercel.app",
   "https://sachipatel-article-hub-frontend.vercel.app"
 ];
 
